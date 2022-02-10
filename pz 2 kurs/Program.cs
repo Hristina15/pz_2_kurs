b5a60bc97a7cfa490b77f28c15213c7fa4ac4c7a
Христина Минеева <h.mineeva@yandex.ru>
2022-02-10 23:13:23 +0300

    Добавьте файлы проекта.

---
+++ pz 2 kurs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz_2_kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Track tra1 = new Track("name", "executor", "album", "year", "bitrate");
+            Track tra2 = new Track("name", "executor", "album", "year", "bitrate", "type");
+            Track tra3 = new Track("name", "executor", "album", "year", "bitrate", "type");
+            Track tra4 = new Track("name", "executor", "album", "year", "type");
+
+            Console.WriteLine(tra1.GetInfo());
+            Console.WriteLine(tra2.GetInfo());
+            Console.WriteLine(tra3.GetInfo());
+            Console.WriteLine(tra4.GetInfo());
+        }
+    }
+}
---
+++ pz 2 kurs/Track.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_2_kurs
+{
+    class Track
+    {
+        public Track(string name, string executor, string album, string year, string bitrate, string type = "музыка")
+        {
+            Singer = executor;
+            Name = name;
+            Album = album;
+            Year = year;
+            Bitrate = bitrate;
+            Type = type;
+
+        }
+        public Track(string name, string executor, string album, string year, string type)
+        {
+            Singer = executor;
+            Name = name;
+            Album = album;
+            Year = year;
+            Bitrate = "16000";
+            Type = type;
+
+        }
+        public string Singer { get; set; }
+        public string Name { get; set; }
+        public string Album { get; set; }
+        public string Year { get; set; }
+
+        public string Bitrate { get; set; }
+
+        public string Type { get; set; }
+
+        public string GetInfo()
+        {
+            return $"Name = {Name}, Singer = {Singer}, Album = {Album}, Year = {Year}, Bitrate = {Bitrate}, Type = {Type}";
+        }
+    }
+}
