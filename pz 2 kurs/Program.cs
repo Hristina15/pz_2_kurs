ba01edbd8a5aca67985dd0512c73cecf429b3f05
Христина Минеева <h.mineeva@yandex.ru>
2022-02-16 21:53:47 +0300

    2.4

--- pz 2 kurs/Program.cs
+++ pz 2 kurs/Program.cs
@@ -6,15 +6,21 @@ namespace pz_2_kurs
     {
         static void Main(string[] args)
         {
-            Track tra1 = new Track("name", "executor", "album", "year", "bitrate");
-            Track tra2 = new Track("name", "executor", "album", "year", "bitrate", "type");
-            Track tra3 = new Track("name", "executor", "album", "year", "bitrate", "type");
-            Track tra4 = new Track("name", "executor", "album", "year", "type");
+            Track tra1 = new Track("name", "executor", "album", "year", TypeEnum.Aac, "16000");
+            Track tra2 = new Track("name", "executor", "album", "year", TypeEnum.Mp3, "32000");
+            Track tra3 = new Track("name", "executor", "album", "year", TypeEnum.Flac, "28000");
+            Track tra4 = new Track("name", "executor", "album", "year", TypeEnum.Aac, "48000");
 
             Console.WriteLine(tra1.GetInfo());
             Console.WriteLine(tra2.GetInfo());
             Console.WriteLine(tra3.GetInfo());
             Console.WriteLine(tra4.GetInfo());
         }
+    }
+    public enum TypeEnum
+    {
+        Mp3,
+        Aac,
+        Flac
     }
 }
--- pz 2 kurs/Track.cs
+++ pz 2 kurs/Track.cs
@@ -8,8 +8,22 @@ namespace pz_2_kurs
 {
     class Track
     {
-        public Track(string name, string executor, string album, string year, string bitrate, string type = "музыка")
+        public Track(string name, string executor, string album, string year, TypeEnum type, string bitrate = "16000")
         {
+
+            if (type == TypeEnum.Aac)
+            {
+                aacCount++;
+            }
+            if (type == TypeEnum.Mp3)
+            {
+                mp3Count++;
+            }
+            if (type == TypeEnum.Flac)
+            {
+                flacCount++;
+            }
+
             Singer = executor;
             Name = name;
             Album = album;
@@ -18,7 +32,7 @@ namespace pz_2_kurs
             Type = type;
 
         }
-        public Track(string name, string executor, string album, string year, string type)
+        public Track(string name, string executor, string album, string year, TypeEnum type)
         {
             Singer = executor;
             Name = name;
@@ -28,6 +42,7 @@ namespace pz_2_kurs
             Type = type;
 
         }
+
         public string Singer { get; set; }
         public string Name { get; set; }
         public string Album { get; set; }
@@ -35,11 +50,22 @@ namespace pz_2_kurs
 
         public string Bitrate { get; set; }
 
-        public string Type { get; set; }
+        public TypeEnum Type { get; set; }
+
+
+        public static int mp3Count = 0;
+        public static int aacCount = 0;
+        public static int flacCount = 0;
+
+        public int getFlac() => flacCount;
+        public int getAac() => aacCount;
+        public int getMp3() => mp3Count;
 
         public string GetInfo()
         {
             return $"Name = {Name}, Singer = {Singer}, Album = {Album}, Year = {Year}, Bitrate = {Bitrate}, Type = {Type}";
         }
+
+        
     }
 }
